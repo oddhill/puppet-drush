389596e5bd1eebb2981012e8c9f8e3b923a94a05
Jens Lind <jens.lind@oddhill.se>
2015-07-02 09:05:00 +0200

    Fixed lint issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,20 +10,20 @@ class drush ($version = '7.0.0') {
   exec {'install-drush':
     command => "bash -c 'source /opt/boxen/env.sh && composer global require drush/drush:${version}'",
     require => Class['php::composer', 'nodejs'],
-    unless => "bash -c 'source /opt/boxen/env.sh && /opt/boxen/repo/shared/drush/files/drush_installed.js ${version}'"
+    unless  => "bash -c 'source /opt/boxen/env.sh && /opt/boxen/repo/shared/drush/files/drush_installed.js ${version}'"
   }
 
   # Install some helpful drush libs
-  exec {"drush-dl-registry-rebuild":
-    command => "drush dl registry_rebuild",
+  exec {'drush-dl-registry-rebuild':
+    command => 'drush dl registry_rebuild',
     creates => "/Users/${::boxen_user}/.drush/registry_rebuild",
-    require => Exec["install-drush"],
+    require => Exec['install-drush'],
   }
 
-  exec {"drush-dl-module-builder":
-    cwd => "/Users/${::boxen_user}/.drush",
-    command => "drush dl module_builder && drush cc drush",
+  exec {'drush-dl-module-builder':
+    cwd     => "/Users/${::boxen_user}/.drush",
+    command => 'drush dl module_builder && drush cc drush',
     creates => "/Users/${::boxen_user}/.drush/module_builder",
-    require => Exec["install-drush"],
+    require => Exec['install-drush'],
   }
 }
