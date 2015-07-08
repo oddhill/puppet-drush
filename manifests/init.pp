adb2540b1cc2cf156a3a84b898e0e79eec9b8ba3
Jens Lind <jens.lind@oddhill.se>
2015-07-08 19:04:51 +0200

    Make sure .drush folder exists

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,7 @@
 # Drush class
 class drush ($version = '7.0.0') {
+  include boxen::config
+
   # Make sure drush is not installed from homebrew
   package {
     'drush':
@@ -11,5 +13,12 @@ class drush ($version = '7.0.0') {
     command => "bash -c 'source /opt/boxen/env.sh && composer global require drush/drush:${version}'",
     require => Class['php::composer', 'nodejs'],
     unless  => "bash -c 'source /opt/boxen/env.sh && /opt/boxen/repo/shared/drush/files/drush_installed.js ${version}'"
+  }
+
+  # Make sure the .drush folder exists
+  file {"/Users/${boxen::config::user}/.drush":
+    ensure => directory,
+    owner  => ${boxen::config::user},
+    group  => staff,
   }
 }
